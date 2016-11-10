27ee1e07010a0a5f172fa3f9779e7157d310a80a
Jon Raymer <jon.raymer@lssinc.com>
2016-11-09 21:33:18 -0600

    Closer

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,19 +1,21 @@
 (function() {
 'use strict';
 
-	angular.module('launchpadApp').component('appComponent', {
-		template: `
-		<div>
-		    <ui-view name="header"></ui-view>
-		    <div class="body-content">
-		        <ui-view name="content"></ui-view>
-		    </div>
-		</div>
-		`,
-		controller: AppController
-	});
+	angular
+		.module('launchpadApp')
+		.component('app', {
+			template: `
+			<div>
+			    <ui-view name="header"></ui-view>
+			    <div class="body-content">
+			        <ui-view name="content"></ui-view>
+			    </div>
+			</div>
+			`,
+			controller: appController
+		});
 
-	function AppController() {}
+	function appController() {}
 })();
 
 
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -13,6 +13,52 @@
 
 	    //Configure default route
 	    $urlRouterProvider.otherwise('/login');
+
+	        //Configure ui states
+	    var states = [
+	    { 
+	        name: 'login', 
+	        url: '/login',
+	        views: {
+	            content: {
+	                // Using component: instead of template:
+	                component: 'login'
+	            },
+	            header: {
+	                component: 'header'
+	            }
+	        }         
+	    },
+	    { 
+	        name: 'register', 
+	        url: '/register',
+	        views: {
+	            content: {
+	                // Using component: instead of template:
+	                component: 'register'
+	            },
+	            header: {
+	                component: 'header'
+	            }
+	        }         
+	    },    
+	    {
+	        name: 'dashboard',
+	        url: '/dashboard',
+	        views: {
+	            content: {                
+	                component: 'dashboard'
+	            },
+	            header: {
+	                component: 'header'
+	            }
+	        }
+	    }];
+
+	    // Loop over the state definitions and register them
+	    states.forEach(function(state) {
+	        $stateProvider.state(state);
+	    });
 	}
 })();
 
