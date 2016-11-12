34d9fdfdc4012907a987d3640472be621b9efe55
Jon Raymer <jon.raymer@lssinc.com>
2016-11-12 14:52:59 -0600

    ES Lint integration

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var useref = require('gulp-useref');
 var lazypipe = require('lazypipe');
 var gulpif = require('gulp-if');
+var eslint = require('gulp-eslint');
 
 var paths = {
   html: ['./src/**/*.html'],
@@ -84,6 +85,23 @@ gulp.task('generate-constants', function() {
     wrap: false
   })
 	.pipe(gulp.dest('./src/app/environment-constants'))
+});
+
+gulp.task('lint', function() {
+    // ESLint ignores files with "node_modules" paths. 
+    // So, it's best to have gulp ignore the directory as well. 
+    // Also, Be sure to return the stream from the task; 
+    // Otherwise, the task may end before the stream has finished. 
+    return gulp.src(paths.js)
+        // eslint() attaches the lint output to the "eslint" property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on 
+        // lint error, return the stream and pipe to failAfterError last. 
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('build', function(done) {
